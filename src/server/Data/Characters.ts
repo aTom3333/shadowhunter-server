ab5eec306b7e0668c9a8702c9b06e6112a25c378
PierreTSE <pierre--giraud@orange.fr>
2019-09-16 14:31:12 +0200

    added full list of characters

--- src/server/Data/Characters.ts
+++ src/server/Data/Characters.ts
@@ -8,7 +8,7 @@ export const characters: Array<Character> = [
         'Franklin',
         Faction.Hunter,
         12,
-        powers.thunder,
+        powers.foudre,
         victoryConditions.hunter
     ),
     // TODO temp
@@ -16,14 +16,162 @@ export const characters: Array<Character> = [
         'Dark Franklin',
         Faction.Shadow,
         12,
-        powers.thunder,
+        powers.foudre,
         victoryConditions.shadow
     ),
     new Character(
         'Neutral Franklin',
         Faction.Neutral,
         12,
-        powers.thunder,
+        powers.foudre,
         victoryConditions.allie
+    ),
+    // auto-generated
+    new Character(
+        "Agnès",
+        Faction.Neutral,
+        8,
+        powers.caprice,
+        victoryConditions.agnes
+    ),
+    new Character(
+        "Allie",
+        Faction.Neutral,
+        8,
+        powers.amourMaternel,
+        victoryConditions.allie
+    ),
+    new Character(
+        "Bob",
+        Faction.Neutral,
+        10,
+        powers.braquage,
+        victoryConditions.bob
+    ),
+    new Character(
+        "Bob",
+        Faction.Neutral,
+        10,
+        powers.braquageExtension,
+        victoryConditions.bob
+    ),
+    new Character(
+        "Bryan",
+        Faction.Neutral,
+        10,
+        powers.ohMyGod,
+        victoryConditions.bryan
+    ),
+    new Character(
+        "Catherine",
+        Faction.Neutral,
+        11,
+        powers.stigmates,
+        victoryConditions.catherine
+    ),
+    new Character(
+        "Charles",
+        Faction.Neutral,
+        11,
+        powers.festinSanglant,
+        victoryConditions.charles
+    ),
+    new Character(
+        "Daniel",
+        Faction.Neutral,
+        13,
+        powers.desespoir,
+        victoryConditions.daniel
+    ),
+    new Character(
+        "David",
+        Faction.Neutral,
+        13,
+        powers.pilleurDeTombes,
+        victoryConditions.david
+    ),
+    new Character(
+        "Ellen",
+        Faction.Hunter,
+        10,
+        powers.exorcisme,
+        victoryConditions.hunter
+    ),
+    new Character(
+        "Emi",
+        Faction.Hunter,
+        10,
+        powers.teleportation,
+        victoryConditions.hunter
+    ),
+    new Character(
+        "Franklin",
+        Faction.Hunter,
+        12,
+        powers.foudre,
+        victoryConditions.hunter
+    ),
+    new Character(
+        "Fu-Ka",
+        Faction.Hunter,
+        12,
+        powers.soinsParticuliers,
+        victoryConditions.hunter
+    ),
+    new Character(
+        "Georges",
+        Faction.Hunter,
+        14,
+        powers.demolition,
+        victoryConditions.hunter
+    ),
+    new Character(
+        "Gregor",
+        Faction.Hunter,
+        14,
+        powers.bouclierFantome,
+        victoryConditions.hunter
+    ),
+    new Character(
+        "Liche",
+        Faction.Shadow,
+        14,
+        powers.necromancie,
+        victoryConditions.shadow
+    ),
+    new Character(
+        "Loup-garou",
+        Faction.Shadow,
+        14,
+        powers.contreattaque,
+        victoryConditions.shadow
+    ),
+    new Character(
+        "Momie",
+        Faction.Shadow,
+        11,
+        powers.rayonDOutremonde,
+        victoryConditions.shadow
+    ),
+    new Character(
+        "Métamorphe",
+        Faction.Shadow,
+        11,
+        powers.imitation,
+        victoryConditions.shadow
+    ),
+    new Character(
+        "Valkyrie",
+        Faction.Shadow,
+        13,
+        powers.chantDeGuerre,
+        victoryConditions.shadow
+    ),
+    new Character(
+        "Vampire",
+        Faction.Shadow,
+        13,
+        powers.morsure,
+        victoryConditions.shadow
     )
 ];
